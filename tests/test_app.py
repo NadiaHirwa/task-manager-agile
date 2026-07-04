06c647a6dd98f9535195e57122cd4146a8e685b5
NadiaHirwa <irahirnad22@gmail.com>
2026-07-04 09:36:02 +0200

    Add tests for POST /tasks: success and missing title

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,3 +16,19 @@ def client(monkeypatch):
 
     if os.path.exists(test_db):
         os.remove(test_db)
+
+def test_create_task_success(client):
+    response =client.post("/tasks", json={"title": "Buy milk"})
+    data = response.get_json()
+
+    assert response.status_code == 201
+    assert data["title"] == "Buy milk"
+    assert data["completed"] is False
+    assert data["id"] is not None
+
+def test_create_task_missing_title(client):
+    response = client.post("/tasks", json={})
+    data = response.get_json()
+
+    assert response.status_code == 400
+    assert "error" in data
